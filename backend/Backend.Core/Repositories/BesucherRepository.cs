fba955960cf6e2509107a3a721167ab80e5130f9
DennisKae <DennisKae@users.noreply.github.com>
2020-03-21 22:18:57 +0100

    Fix: Die Personendaten eines Besuchers eines Besuchs wurden nicht ausgegeben

--- backend/Backend.Core/Repositories/BesucherRepository.cs
+++ backend/Backend.Core/Repositories/BesucherRepository.cs
@@ -24,8 +24,8 @@ namespace Backend.Core.Repositories
         {
             return _databaseContext.BesuchBesucher
                 .Where(x => besuche.Select(y => y.Id).Contains(x.BesuchId) && x.Besucher != null)
+                .Include(x => x.Besucher.Person)
                 .Select(x => x.Besucher)
-                //.Include(x => x.Person)
                 .ToList();
         }
 
@@ -33,8 +33,8 @@ namespace Backend.Core.Repositories
         {
             return _databaseContext.BesuchBesucher
                 .Where(x => x.BesuchId == besuch.Id && x.Besucher != null)
+                .Include(x => x.Besucher.Person)
                 .Select(x => x.Besucher)
-                //.Include(x => x.Person)
                 .ToList();
         }
     }
